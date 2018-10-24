172e9f9bf4b76144492c0b6a5140dc2680c99587
MiriamPeskowitz <miriam5000@gmail.com>
2018-10-23 21:53:34 -0400

    works with layout, trying to fix why the nav bar won't work, some changes to asset pipeline

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,13 +1,21 @@
-class Comments {
-  constructor() {
-    this.title = title 
-    this.content = content
-    this.soupkitchen_id = soupkitchen_id
-    this.user_id = user_id
-    this.created_at = Date.parse(obj.created_at);
-    this.updated_at = Date.parse(obj.updated_at);
+$(function(){
+
+
+  class Comments {
+    constructor() {
+      this.title = title 
+      this.content = content
+      this.soupkitchen_id = soupkitchen_id
+      this.user_id = user_id
+      this.created_at = Date.parse(obj.created_at);
+      this.updated_at = Date.parse(obj.updated_at);
+    }
+    // Comment.prototype.XXXX = function() {
+    //   console.log("XXXX");
+    // }
   }
-  // Comment.prototype.XXXX = function() {
-  //   console.log("XXXX");
-  // }
-}
+
+
+
+
+})
--- app/assets/javascripts/soupkitchens.js
+++ app/assets/javascripts/soupkitchens.js
@@ -1,13 +1,19 @@
-class Soupkitchen {
-  constructor() {
-    this.name = name 
-    this.address = address
-    this.zipcode = zipcode
-    this.notes = notes
-    this.created_at = Date.parse(obj.created_at);
-    this.updated_at = Date.parse(obj.updated_at);
+$(function(){
+
+
+  class Soupkitchen {
+    constructor() {
+      this.name = name 
+      this.address = address
+      this.zipcode = zipcode
+      this.notes = notes
+      this.created_at = Date.parse(obj.created_at);
+      this.updated_at = Date.parse(obj.updated_at);
+    }
+    // Soupkitchen.prototype.XXXX = function() {
+    //   console.log("XXXX");
+    // } -- can be a formating method 
   }
-  // Soupkitchen.prototype.XXXX = function() {
-  //   console.log("XXXX");
-  // }
-}
+
+
+})
