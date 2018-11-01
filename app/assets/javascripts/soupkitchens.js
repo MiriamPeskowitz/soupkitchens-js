76c6ae38742e278f9b8b5358578537bbed1fd9df
MiriamPeskowitz <miriam5000@gmail.com>
2018-11-01 17:47:14 -0400

    revert js, set up basic button hijack

--- app/assets/javascripts/soupkitchens.js
+++ app/assets/javascripts/soupkitchens.js
@@ -1,7 +1,4 @@
-$(function(){
-
-
-  class Soupkitchen {
+class Soupkitchen {
     constructor() {
       this.name = name 
       this.address = address
@@ -15,5 +12,19 @@ $(function(){
     // } -- can be a formating method 
   }
 
+$(document).ready(function() {
 
-})
+    $('#js-soupkitchen-button').on("click", 
+    getSoupkitchens);
+  
+  function getSoupkitchens(event) {
+    event.preventDefault();
+    console.log("got to soupkitchens")
+    fetch('/soupkitchens/index')
+    .then(response => response.json())
+    .then(data => console.log(data))
+  }
+
+});
+
+// this is closing bracket
