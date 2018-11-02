67980ccc5967b2ab0a3a1a557d041ec72d6a5bd0
MiriamPeskowitz <miriam5000@gmail.com>
2018-11-02 14:31:05 -0400

    partial code for fetch

--- app/assets/javascripts/soupkitchens.js
+++ app/assets/javascripts/soupkitchens.js
@@ -1,9 +1,9 @@
 class Soupkitchen {
     constructor() {
-      this.name = name 
-      this.address = address
-      this.zipcode = zipcode
-      this.notes = notes
+      this.name = name; 
+      this.address = address;
+      this.zipcode = zipcode;
+      this.notes = notes;
       this.created_at = Date.parse(obj.created_at);
       this.updated_at = Date.parse(obj.updated_at);
     }
@@ -19,21 +19,49 @@ $(document).ready(function() {
   
   function getSoupkitchens(event) {
     event.preventDefault();
-    console.log("got to soupkitchens")
+            console.log("got to soupkitchens")
     fetch('/soupkitchens')
-    .then(response => console.log(response.text()))
-    .then(response => response.json())
+        // .then(console.log("got to fetch"))
+    .then(function(res) {
+      return res.text();
+    })
+    .then(text => console.log(text))
+    // .then(function(data){
+    //   console.log(JSON.stringify(data))
+    // })
 
-    .then(console.log("got to 2d promise-replace this with a function "))
-    // .then(data => console.log(data))
-    .catch(error => console.log(error))
+    // .then(response => response.json() )
+
+      // .then(console.log("got to 2d promise-replace this with a function " + res))
+  .then(data => putSoupKitchenInDom)
+  .catch(error => console.log(error))
   }
 
 function putSoupKitchenDataInDom() {
-  
-  $('#soupkitchen-data'). 
-}
+  console.log("got to putSoupKitchenInDome")
+  let result = `data for each soupkitchen`;
+    data.forEach(function(soupkitchen) {
+      const {name, address, zipcode } = soupkitchen;
+      result +=
+      `<div>
+          <h5> Name: ${name} </h5>
+          <ul>   
+              <li>Address: ${address} </li>
+              <li>Address: Philadelphia, PA ${zipcode} </li>
+              <li><button id="js-meal-schedule">Meal Schedule/add id thing</button>Goes to ${notes}</li>
+              <li><button id="js-see-reviews">See Reviews (and login to leave your own)${comments}</li>
+          </ul>      
+            <div id="js-load-meal-schedule"></div>
+            <div id="js-load-reviews"></div>
+      </div> `
+    });
 
+
+  $('#soupkitchen-data').innerHTML = result;
+};
+
+
+//after this, do reviews,return to home. 
 });
 
 // this is closing bracket
