ac118032a2504196c93d3af254fb8a2406b7cafe
MiriamPeskowitz <miriam5000@gmail.com>
2018-11-10 23:41:31 -0500

    update bundle

--- config/application.rb
+++ config/application.rb
@@ -30,8 +30,8 @@ module Soupkitchens-js
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
-    config.middleware.use ActionDispatch::Flash
+    # config.api_only = true
+    # config.middleware.use ActionDispatch::Flash
 
     config.generators do |g|
       g.javascript_engine :js
