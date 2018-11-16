167081ee8ccb4b57ee0553f28f71f20eb7ab3627
MiriamPeskowitz <miriam5000@gmail.com>
2018-11-16 13:08:50 -0500

    add more code to foodpantries.js

--- app/assets/javascripts/foodpantries.js
+++ app/assets/javascripts/foodpantries.js
@@ -1,10 +1,10 @@
 
 class Foodpantry {
-  constructor() {
-    this.name = name 
-    this.address = address
-    this.zipcode = zipcode
-    this.notes = notes
+  constructor(attr) {
+    this.name = attr.name;
+    this.address = attr.address;
+    this.zipcode = attr.zipcode;
+    this.notes = attr.notes;
     // this.created_at = Date.parse(obj.created_at);
     // this.updated_at = Date.parse(obj.updated_at);
   }
@@ -13,33 +13,65 @@ class Foodpantry {
   // }
 }
 
+ Foodpantry.prototype.formatHTML = function() {
+   
+     return `<p> 
+            ${this.name}<br>
+            ${this.address}<br>
+            ${this.zipcode}<br>
+            ${this.notes}<br>
+            <button data-id=${this.id} id="reviews-${this.id}" >
+                Button
+            </button> 
+            </p>`
+    }
+
 // $('#js-foodpantry-button').on("click", 
 //     getFoodPantries);
   
 function  foodpantriesFetch() {
+
+    const indexRequest = new Request('/foodpantries', {
+       headers: new Headers({
+      'Content-Type': 'application/json'
+        })
+    })
     clearSoupKitchenDataAndTitle();
+
+    addFoodpantriesTitle();
 
     $('#foodpantry-data').append("Coming Soon");
   
-    addFoodpantriesTitle();
+   
 
-    // fetch('/foodpantries')
-    // .then(response => response.json())
-    // .then(console.log("got to 2d promise"))
-    // .then(data => console.log(data))
-  }
+    // fetch(indexRequest)
+    // .then(res => res.json())
+    // .then(console.log("got to res.json"))
+    // .then(data => {
+      // console.log(data)
+      // const foodpantries = data;
+
+      // foodpantries.forEach(function(foodpantry)
+      //   ){
+      //   const pantry = new Foodpantry(foodpantry)
+      // }
+      //  $('#foodpantry-data').append(pantry.formatHTML())
+      // clickbutton for rviews 
+    // })
+
+
+  } //end bracket for fetch function 
 function addFoodpantriesTitle() {
     const foodpantriesTitle = `<h4> Food Pantries </h4>`;
     const $foodpantryTitleDiv = $('#foodpantry-title');
     if ($foodpantryTitleDiv.empty() ) {
       ($foodpantryTitleDiv).prepend(foodpantriesTitle);
     } 
-  }
+}
 
-  function clearSoupKitchenDataAndTitle() {
-    console.log("got to clearSoupKitchens")
+function clearSoupKitchenDataAndTitle() {
     $('#soupkitchen-title').html("");
     $('#soupkitchen-data').html("");
-  }
+}
 
 
